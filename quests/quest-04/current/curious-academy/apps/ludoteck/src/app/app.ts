c848b6b45f12ac84a07389d9388dee99490d83c1
qdimanche <73292719+qdimanche@users.noreply.github.com>
2026-02-10 09:21:49 +0100

    feat: add boostrap

--- quests/quest-04/current/curious-academy/apps/ludoteck/src/app/app.ts
+++ quests/quest-04/current/curious-academy/apps/ludoteck/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ListVideoGames } from '../features/video-games/components/list-video-games/list-video-games';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import '@angular/localize/init';
 
 @Component({
-  imports: [ListVideoGames],
+  imports: [ListVideoGames, NgbAlert],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
