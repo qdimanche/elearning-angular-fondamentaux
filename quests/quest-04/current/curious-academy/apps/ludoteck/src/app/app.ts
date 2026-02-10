3d70fba07e0441cac35f3b34979bf24f8206efd3
qdimanche <73292719+qdimanche@users.noreply.github.com>
2026-02-10 09:36:10 +0100

    chore: install material

--- quests/quest-04/current/curious-academy/apps/ludoteck/src/app/app.ts
+++ quests/quest-04/current/curious-academy/apps/ludoteck/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ListVideoGames } from '../features/video-games/components/list-video-games/list-video-games';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
-  imports: [ListVideoGames],
+  imports: [ListVideoGames, MatSliderModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
